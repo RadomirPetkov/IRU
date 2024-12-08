9eb631f6a7232767f19f4ccf864f397c520e2a50
Radomir Petkov <105939480+RadomirPetkov@users.noreply.github.com>
2024-12-08 10:14:34 +0200

    initial commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,15 +9,14 @@ export const Header = (props) => {
             <div className="row">
               <div className="col-md-8 col-md-offset-2 intro-text">
                 <h1>
-                  {props.data ? props.data.title : "Loading"}
-                  <span></span>
+                  <span>{props.data ? props.data.title : "Loading"}</span>
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
                 <a
                   href="#features"
                   className="btn btn-custom btn-lg page-scroll"
                 >
-                  Learn More
+                  Научете повече
                 </a>{" "}
               </div>
             </div>
